9b9446da485f643d4ba7092e1b27895e86e385cf
varun406 <varunnayak46@gmail.com>
2022-07-15 12:36:04 +0530

    added images

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,94 +1,93 @@
-import React from 'react'
-import styled from "styled-components"
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
-function Section({title,description,leftBtnText,rightBtnText,backgroundImg}) {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap bgImage = {backgroundImg} >
-        <Fade bottom>
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
         <ItemText>
-            <h1>{title}</h1>
-            <p>{description}</p>
+          <h1>{title}</h1>
+          <p>{description}</p>
         </ItemText>
-        </Fade>
-        
+      </Fade>
 
-    <Fade bottom>
-        <Buttons> 
-        <ButtonGroup>
-                <LeftButton>
-                    {leftBtnText}
-                </LeftButton>
-                {rightBtnText && 
-                    <RightButton>
-                {rightBtnText}
-                </RightButton>
-                }
-                
-            </ButtonGroup>
-            <DownArrow src="/images/down-arrow.svg"/>
+      <Fade bottom>
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
         </Buttons>
-    </Fade>    
+      </Fade>
     </Wrap>
-  )
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-    width:100vw;
-    height:100vh;
-    background-size:cover;
-    background-position:center;
-    background-repeat:no-repeat;
-    background-image: ${props => `url("/images/${props.bgImage}")`};
-    display:flex;
-    flex-direction:column;
-    justify-content:space-between; //vertical alignment
-    align-items:center; //horizontal alignment
-`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical alignment
+  align-items: center; //horizontal alignment
+`;
 
 const ItemText = styled.div`
-padding-top:15vh;
-text-align:center
-`
+  padding-top: 15vh;
+  text-align: center;
+`;
 
 const ButtonGroup = styled.div`
-    display:flex;
-    margin-bottom:30px;
-    
-    @media (max-width:768px) {
-        flex-direction:column;
-    }
-`
-const LeftButton = styled.div`
-    background-color:rgba(23,26,32,0.8);
-    height:40px;
-    width:256px;
-    color:white;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    border-radius:100px;
-    opacity:0.85;
-    text-transform:uppercase;
-    font-size:12px;
-    cursor:pointer;
-    margin:8px;
-`
+  display: flex;
+  margin-bottom: 30px;
 
-const RightButton = styled(LeftButton)`  //helps to inherit css of leftbutton
-    background-color:white;
-    opacity:0.65;
-    color:black;
-`
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  //helps to inherit css of leftbutton
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`;
 
 const DownArrow = styled.img`
-    height:40px;
-    animation:animateDown infinite 1.5s;
-    overflow-x:hidden;
-`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
 
-const Buttons =styled.div`
-    margin-bottom:10px;
-`
+const Buttons = styled.div`
+  margin-bottom: 10px;
+`;
